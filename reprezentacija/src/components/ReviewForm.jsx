4908ce3b95d77f5ef57910ac1c8b433404e4bbc3
Zigmas Petrauskas <zigmas.1@gmail.com>
2024-11-06 08:49:44 +0200

    xdgbhcfgjfg

--- reprezentacija/src/components/ReviewForm.jsx
+++ reprezentacija/src/components/ReviewForm.jsx
@@ -29,33 +29,40 @@ const ReviewForm = ({ onSubmit }) => {
           });
       }}
     >
-      <Form className="review-form">
-        <h2>Palikite Atsiliepimą</h2>
-        <div className="form-group">
-          <label htmlFor="name">Vardas</label>
-          <Field name="name" type="text" placeholder="Įveskite savo vardą" />
-        </div>
-        <div className="form-group">
-          <label>Įvertinimas</label>
-          <div className="rating-group">
-            {[1, 2, 3, 4, 5].map((star) => (
-              <label key={star} className="rating-bubble">
-                <Field type="radio" name="rating" value={star} />
-                <span>{star}</span>
-              </label>
-            ))}
+      {({ setFieldValue }) => (
+        <Form className="review-form">
+          <h2>Palikite Atsiliepimą</h2>
+          <div className="form-group">
+            <label htmlFor="name">Vardas</label>
+            <Field name="name" type="text" placeholder="Įveskite savo vardą" />
           </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="comment">Atsiliepimas</label>
-          <Field
-            name="comment"
-            as="textarea"
-            placeholder="Parašykite savo atsiliepimą"
-          />
-        </div>
-        <button type="submit" className="submit-button">Siųsti atsiliepimą</button>
-      </Form>
+          <div className="form-group">
+            <label>Įvertinimas</label>
+            <div className="rating-group">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <label key={star} className="rating-bubble">
+                  <Field 
+                    type="radio" 
+                    name="rating" 
+                    value={star} 
+                    onChange={() => setFieldValue("rating", star)} // Pridėta logika, kad pažymėtų vertinimą
+                  />
+                  <span>{star}</span>
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="comment">Atsiliepimas</label>
+            <Field
+              name="comment"
+              as="textarea"
+              placeholder="Parašykite savo atsiliepimą"
+            />
+          </div>
+          <button type="submit" className="submit-button">Siųsti atsiliepimą</button>
+        </Form>
+      )}
     </Formik>
   );
 };
